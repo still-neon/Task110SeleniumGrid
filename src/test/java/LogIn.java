5d29ae75d850a86ac8b246783595169eedd690a8
Eugene Krasotkin <still-neon@tut.by>
2017-05-15 12:25:53 +0300

    Initial commit 01

--- src/test/java/LogIn.java
+++ src/test/java/LogIn.java
@@ -5,9 +5,12 @@ import org.testng.annotations.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static org.testng.Assert.assertEquals;
+
 public class LogIn {
 
     private WebDriver driver;
+    private String TITLE = "Mail.Ru: почта, поиск в интернете, новости, игры";
 
     @BeforeMethod
     public void setUp() throws MalformedURLException {
@@ -19,7 +22,7 @@ public class LogIn {
     public void login() throws Exception{
 
         driver.get("https://mail.ru/");
+        assertEquals(driver.getTitle(), TITLE);
 
-        //System.out.println(driver.getTitle());
     }
 }
